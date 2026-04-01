f01490a5dd6062de11271be564a7b8551f4e1c3e
bjw446 <bjw446@naver.com>
2026-04-01 18:59:48 +0900

    feat : 유저 멤버쉽 검증 추가

--- src/main/java/com/example/coffee_shop_project/domain/user/controller/UserController.java
+++ src/main/java/com/example/coffee_shop_project/domain/user/controller/UserController.java
@@ -1,11 +1,26 @@
 package com.example.coffee_shop_project.domain.user.controller;
 
+import com.example.coffee_shop_project.common.dto.CommonResponse;
+import com.example.coffee_shop_project.common.enums.SuccessStatus;
+import com.example.coffee_shop_project.domain.user.dto.MembershipRequest;
+import com.example.coffee_shop_project.domain.user.dto.MembershipResponse;
 import com.example.coffee_shop_project.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/membership")
+    public ResponseEntity<CommonResponse<MembershipResponse>> membershipLookup (@Valid @RequestBody MembershipRequest request) {
+        return ResponseEntity.ok(CommonResponse.success(SuccessStatus.READ_SUCCESS, userService.membershipLookup(request)));
+    }
 }
--- src/main/java/com/example/coffee_shop_project/domain/user/service/UserService.java
+++ src/main/java/com/example/coffee_shop_project/domain/user/service/UserService.java
@@ -1,11 +1,35 @@
 package com.example.coffee_shop_project.domain.user.service;
 
+import com.example.coffee_shop_project.domain.user.dto.MembershipRequest;
+import com.example.coffee_shop_project.domain.user.dto.MembershipResponse;
+import com.example.coffee_shop_project.domain.user.entity.User;
 import com.example.coffee_shop_project.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public MembershipResponse membershipLookup(MembershipRequest request) {
+        Optional<User> user = Optional.empty();
+
+        if (request.getMembershipNumber() != null) {
+            user = userRepository.findByMembershipNumber(request.getMembershipNumber());
+        }
+
+        if (user.isEmpty()) {
+            return MembershipResponse.nonMember();
+        }
+
+        User existsUser = user.get();
+
+        return MembershipResponse.isMember(existsUser);
+    }
 }
