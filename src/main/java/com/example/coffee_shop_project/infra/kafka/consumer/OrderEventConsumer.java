0fe849e64d01d7b5a7200a2745195042976667ef
bjw446 <bjw446@naver.com>
2026-04-03 04:36:05 +0900

    feat : 컨슈머 실패 시 로그 추가

--- src/main/java/com/example/coffee_shop_project/infra/kafka/consumer/OrderEventConsumer.java
+++ src/main/java/com/example/coffee_shop_project/infra/kafka/consumer/OrderEventConsumer.java
@@ -2,12 +2,16 @@ package com.example.coffee_shop_project.infra.kafka.consumer;
 
 import com.example.coffee_shop_project.domain.menusales.service.MenuSalesService;
 import com.example.coffee_shop_project.domain.order.dto.OrderCreatedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderEventConsumer {
@@ -15,9 +19,16 @@ public class OrderEventConsumer {
     private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "order-events", groupId = "menu-sales-group", concurrency = "1")
-    public void consume(String message) throws Exception {
-        OrderCreatedEvent event = objectMapper.readValue(message, OrderCreatedEvent.class);
+    @Transactional
+    public void consume(String message) throws JsonProcessingException {
+        try {
+            OrderCreatedEvent event = objectMapper.readValue(message, OrderCreatedEvent.class);
 
-        menuSalesService.updateSales(event);
+            menuSalesService.updateSales(event);
+        } catch (Exception e) {
+            log.error("Kafka consume 실패", e);
+            throw e;
+        }
+
     }
 }
